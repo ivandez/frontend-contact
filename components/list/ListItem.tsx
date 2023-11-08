226531a0fcc8b9a5d4125bfd3689375db31fd939
ivandez <ivan.mendez.dev@gmail.com>
2023-11-08 16:55:36 -0400

    delete contact

--- components/list/ListItem.tsx
+++ components/list/ListItem.tsx
@@ -1,8 +1,17 @@
 import { Button } from "flowbite-react";
 import { Contact } from "./domain";
 import Link from "next/link";
+import axios from "axios";
 
 const ListItem = ({ name, id }: Contact) => {
+  const deleteContact = async (id: any) => {
+    try {
+      axios.delete(`http://localhost:3000/v1/agenda/${id}`);
+      alert("deleted");
+    } catch (error) {
+      alert("Can not delete this contact");
+    }
+  };
   return (
     <div className="grid grid-cols-2 items-center">
       <span>{name}</span>
@@ -13,7 +22,9 @@ const ListItem = ({ name, id }: Contact) => {
         <Button color="yellow">
           <Link href={`/dashboard/${id}/edit`}>edit</Link>
         </Button>
-        <Button color="red">delete</Button>
+        <Button color="red" onClick={() => deleteContact(id)}>
+          delete
+        </Button>
       </div>
     </div>
   );
