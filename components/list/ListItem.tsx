193cad8df02b89b812778f17e5b1ff872fab607e
ivandez <ivan.mendez.dev@gmail.com>
2023-11-08 13:48:18 -0400

    end link to view contact

--- components/list/ListItem.tsx
+++ components/list/ListItem.tsx
@@ -1,12 +1,15 @@
 import { Button } from "flowbite-react";
 import { Contact } from "./domain";
+import Link from "next/link";
 
-const ListItem = ({ name }: Contact) => {
+const ListItem = ({ name, id }: Contact) => {
   return (
     <div className="grid grid-cols-2 items-center">
       <span>{name}</span>
       <div className="flex gap-2">
-        <Button color="blue">view</Button>
+        <Button color="blue">
+          <Link href={`/dashboard/${id}`}>view</Link>
+        </Button>
         <Button color="yellow">edit</Button>
         <Button color="red">delete</Button>
       </div>
