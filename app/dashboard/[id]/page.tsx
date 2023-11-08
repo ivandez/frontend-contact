0852690ee20fd60c6f6f16368def3f296c34c6a4
ivandez <ivan.mendez.dev@gmail.com>
2023-11-08 13:57:59 -0400

    end view view

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import NavigationBar from "@/components/navbar/NavigationBar";
+import { Button } from "flowbite-react";
+
 export default function Page({ params }: { params: { id: string } }) {
-  return <div>My Post: {params.id}</div>;
+  return (
+    <>
+      <NavigationBar />
+      <div className="container mx-auto px-4 mt-2 flex flex-col items-center">
+        <div className="flex gap-6 flex-col mb-4">
+          <h1>Name</h1>
+          <h2>email</h2>
+          <h2>email</h2>
+        </div>
+        <div className="flex gap-2">
+          <Button color="blue">back</Button>
+          <Button color="yellow">Edit</Button>
+          <Button color="red">delete</Button>
+        </div>
+      </div>
+    </>
+  );
 }
